20a748f68b0f6f5d2a158816bce5ff5e01867afc
qurriahSam <mugirethe1@gmail.com>
2023-02-27 17:42:14 +0300

    st: add static files for css

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ let express = require("express");
 let app = express();
 
 const filepath = `${__dirname}/views/index.html`;
+const statFiles = express.static(`${__dirname}/public`);
+
+app.use("/public", statFiles);
 
 app.get("/", (req, res) => {
   res.status(200).sendFile(filepath);
