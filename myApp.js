bc3f6ebfb1c48b8e7cd767bffe006732d85fe3de
qurriahSam <mugirethe1@gmail.com>
2023-02-27 20:17:07 +0300

    ch: use env variable to change message style

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
+
+const isUpcase = process.env.MESSAGE_STYLE;
+console.log(isUpcase);
 
 const filepath = `${__dirname}/views/index.html`;
 const statFiles = express.static(`${__dirname}/public`);
@@ -10,8 +14,13 @@ app.get("/", (req, res) => {
   res.status(200).sendFile(filepath);
 });
 
+let message = "Hello json";
+if (isUpcase === "uppercase") {
+  message = message.toUpperCase();
+}
+
 app.get("/json", (req, res) => {
-  res.status(200).json({ message: "Hello json" });
+  res.status(200).json({ message: message });
 });
 
 module.exports = app;
