f1637271a3753603e73249ceacaf5449f558ea52
qurriahSam <mugirethe1@gmail.com>
2023-02-27 18:17:32 +0300

    ch: add json path

--- myApp.js
+++ myApp.js
@@ -10,4 +10,8 @@ app.get("/", (req, res) => {
   res.status(200).sendFile(filepath);
 });
 
+app.get("/json", (req, res) => {
+  res.status(200).json({ message: "Hello json" });
+});
+
 module.exports = app;
