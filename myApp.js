c98ba456781f00c11a81e7ad50d9f7805f997b72
qurriahSam <mugirethe1@gmail.com>
2023-02-28 17:50:12 +0300

    ch: create timeserver middlware

--- myApp.js
+++ myApp.js
@@ -27,4 +27,15 @@ app.get("/json", (req, res) => {
   res.status(200).json({ message: message });
 });
 
+application.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+
 module.exports = app;
