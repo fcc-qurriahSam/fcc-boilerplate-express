c6ef4adbbedc1ccd3e6c7db159c0d4d348818d95
qurriahSam <mugirethe1@gmail.com>
2023-02-28 19:52:47 +0300

    ch: use query param

--- myApp.js
+++ myApp.js
@@ -42,4 +42,10 @@ app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
 
+app.route("/name").get((req, res) => {
+  const first = req.query.first;
+  const last = req.query.last;
+  res.json({ name: `${first} ${last}` });
+});
+
 module.exports = app;
